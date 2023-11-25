53f802af6e45b96e731cbed297d3f13d01250a10
unknown <nikmatulaulina6@gmail.com>
2023-11-25 09:03:28 +0700

    dua

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const btnView = document.querySelector("#btn-view");
+btnView.addEventListener("click", () => {
+    window.open("source/APS1.pptx");
+})
+
+const btnCv = document.querySelector("#btn-cv");
+btnCv.addEventListener("click", () => {
+    window.open("source/CV.pdf");
+})
+
+const linked = document.querySelector("#linked");
+linked.addEventListener("click", () => {
+    window.open("https://linkedin.com/in/aulina-nikmatul");
+})
+
+const instagram = document.querySelector("#instagram");
+instagram.addEventListener("click", () => {
+    window.open("https://www.instagram.com/aulinamuzakki/");
+})
+
+const btnGithub = document.querySelector("#btn-github");
+btnGithub.addEventListener("click", () => {
+    window.open("https://github.com/aulina");
+})
+
+const btnView2 = document.querySelector("#btn-view2");
+btnView2.addEventListener("click", () => {
+    window.open("https://dekadii01.github.io/Porto-team-PWPB/");
+})
